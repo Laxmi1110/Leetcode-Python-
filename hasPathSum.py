a307df64dc49ad137fcf137d64599e8c2810fa31
Laxmi Gupta <laxmigupta188777@gmail.com>
2026-06-09 16:25:27 +0530

    Implement hasPathSum function for binary tree

---
+++ hasPathSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasPathSum(self, root, targetSum):
+        if not root:
+            return False
+
+        # Leaf node
+        if not root.left and not root.right:
+            return targetSum == root.val
+
+        return (self.hasPathSum(root.left, targetSum - root.val) or
+                self.hasPathSum(root.right, targetSum - root.val))
